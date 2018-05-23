ef8ad5804f72f6f9237539295c0e4194e70b6c97
aktiger <uestczhangchao@gmail.com>
2018-05-23 13:51:05 +0800

    add filter main func logic

--- test/testImg2Array7x7.cpp
+++ test/testImg2Array7x7.cpp
@@ -10,12 +10,30 @@
 #include <limits.h>
 #include <unistd.h>
 #include "buffer.h"
+#include "YoloOCLDNN.h"
+
+YOLONeuralNet	*m_YOLODeepNNObj;
 
 BOOST_AUTO_TEST_CASE(FailTest) {
     BOOST_CHECK_EQUAL(1,2);
 }
 
 BOOST_AUTO_TEST_CASE(PassTest) {
+
+    string	labelsFile = "/home/ubuntu/zhangchao/cvs/YoloOCLInference/data/coco.names";
+    string	configFile = "/home/ubuntu/zhangchao/cvs/YoloOCLInference/data/tiny-yolo.cfg";
+    string	weightsFile = "/home/ubuntu/zhangchao/cvs/YoloOCLInference/data/tiny-yolo.weights";
+    char inputImage[PATH_MAX] =  "/home/ubuntu/zhangchao/cvs/YoloOCLInference/data/pedestrians.jpg";
+
+    int		enableDisplay = 1;
+    int		saveOutput = 0;
+
+    m_YOLODeepNNObj = new YOLONeuralNet(&labelsFile[0u], &configFile[0u], &weightsFile[0u], enableDisplay, saveOutput);
+    m_YOLODeepNNObj->Initialize();
+    m_YOLODeepNNObj->ComputeYOLONNOutput(inputImage);
+
+    m_YOLODeepNNObj->Finalize();
+    delete m_YOLODeepNNObj;
     BOOST_CHECK_EQUAL(1,1);
 }
 
